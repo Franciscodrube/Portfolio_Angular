2cec1c9eb33a112fd237015bdc7178fe5877b7f1
franciscodrube <franciscodrube@gmail.com>
2022-04-26 19:56:07 -0300

    Creacion de componentes, configuración y traslado desde el archivo original html y desde el css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { HomeComponent } from './componentes/home/home.component';
+import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
+import { EducacionComponent } from './componentes/educacion/educacion.component';
+import { SkillsComponent } from './componentes/skills/skills.component';
+import { PortfolioComponent } from './componentes/portfolio/portfolio.component';
+import { ContactoComponent } from './componentes/contacto/contacto.component';
+import { FooterComponent } from './componentes/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    ExperienciaComponent,
+    EducacionComponent,
+    SkillsComponent,
+    PortfolioComponent,
+    ContactoComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
